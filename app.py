a25b08b300a4a27d0ca2702eb090f3390bfc7bd1
izzeddincelik <izzeddinmupe@gmail.com>
2023-02-03 00:21:40 +0300

    updated

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, render_template
+import PyPDF2
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/pdf_upload", methods=["POST"])
+def pdf_upload():
+    file = request.files["pdf_file"]
+    pdf = PyPDF2.PdfReader(file)
+    words = []
+    num_pages = len(pdf.pages)
+    for page_num in range(num_pages):
+        page = pdf.pages[page_num]
+        if 'İl' in words:
+            continue
+        else:
+            words += page.extract_text().split(" ")
+    return render_template("words.html", words=words)
+
+if __name__ == "__main__":
+    app.run()
