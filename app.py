a92e0dd701302aee1b4766d2cf74919aeebff3b6
izzeddincelik <izzeddinmupe@gmail.com>
2023-02-03 13:15:39 +0300

    test

--- app.py
+++ app.py
@@ -10,10 +10,11 @@ def index():
 @app.route("/pdf_upload", methods=["POST"])
 def pdf_upload():
     file = request.files["pdf_file"]
-    
+
     pdf = PyPDF2.PdfReader(file)
     words = []
     num_pages = len(pdf.pages)
+    
     for page_num in range(num_pages):
         page = pdf.pages[page_num]
         words += page.extract_text().split("\n")
