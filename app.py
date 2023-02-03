1212f8b8fe8745a55336c21efc61f25c9d73d4aa
İzzedin Çelik <izzeddinmupe@gmail.com>
2023-02-03 10:48:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,4 @@ def pdf_upload():
     return render_template("words.html", words=words)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80) 
+    app.run(host='0.0.0.0', port=5000) 
